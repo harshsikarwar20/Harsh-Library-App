fc59fe46819828bc7f98d64fbc7f321df099edbe
Harsh Sikarwar <123385605+harshsikarwar20@users.noreply.github.com>
2023-02-22 21:47:34 +0530

    Create LibraryApplication.java

---
+++ LibraryApplication.java
@@ -0,0 +1,13 @@
+package com.Harsh.LibraryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+		System.out.println("Successfully Run");
+	}
+}
