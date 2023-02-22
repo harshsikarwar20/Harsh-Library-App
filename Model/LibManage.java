8a28158ff2a92f0f91211006b2f6cbf12f889336
Harsh Sikarwar <123385605+harshsikarwar20@users.noreply.github.com>
2023-02-22 21:45:10 +0530

    Create LibManage.java

---
+++ Model/LibManage.java
@@ -0,0 +1,15 @@
+package com.Harsh.LibraryApp.model;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class LibManage {
+    private String name;
+    private String address;
+    private int number;
+    private int faculties;
+}
